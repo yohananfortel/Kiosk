b01a5b98063ae2907b73753cea671de5154f9fd6
Vladimir <volodimirroskosnij@gmail.com>
2026-05-13 15:36:46 +0300

    Видалив не потрвбні схеми

--- src/components/WellcomeToTheCollege.vue
+++ src/components/WellcomeToTheCollege.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="dashboard-container">
-    
     <div class="tiles-grid">
-      <div 
-        v-for="item in menuItems" 
-        :key="item.id" 
+      <div
+        v-for="item in menuItems"
+        :key="item.id"
         class="tile-card"
         :style="{ backgroundColor: item.color }"
         @click="openModal(item)"
@@ -12,11 +11,11 @@
         <div class="icon-wrapper">
           <span class="flat-icon">{{ item.icon }}</span>
 
-          <img 
-            v-if="item.image" 
-            :src="item.image" 
-            :alt="item.title" 
-            class="flat-image" 
+          <img
+            v-if="item.image"
+            :src="item.image"
+            :alt="item.title"
+            class="flat-image"
           />
         </div>
         <h3 class="tile-title">{{ item.title }}</h3>
@@ -27,33 +26,66 @@
       <div v-if="isModalOpen" class="modal-overlay" @click.self="closeModal">
         <div class="modal-content">
           <button class="close-btn" @click="closeModal">&times;</button>
-          
+
           <component :is="activeComponent" />
         </div>
       </div>
     </Transition>
-
-
   </div>
 </template>
 
 <script setup>
-import { ref, shallowRef } from 'vue';
-import Abiturientu from './Abiturientu.vue';
-import Studentu from './Studentu.vue'
-import Shelter from './Shelter.vue';
-import Contacts from './Contacts.vue';
-import Dormitory from './Dormitory.vue';
+import { ref, shallowRef } from "vue";
+import Abiturientu from "./Abiturientu.vue";
+import Studentu from "./Studentu.vue";
+import Shelter from "./Shelter.vue";
+import Contacts from "./Contacts.vue";
+import Dormitory from "./Dormitory.vue";
 
 const isModalOpen = ref(false);
 const activeComponent = shallowRef(null);
 
 const menuItems = ref([
-  { id: 1, title: 'Абітурієнту', image: 'img/abiturient.png', color: '#00a53f', link: '/admission', component: Abiturientu },
-  { id: 2, title: 'Студенту',  image: 'img/student.png', color: '#00a53f', link: '/student', component: Studentu},
-  { id: 3, title: 'Контакти', image: 'img/telephone-call.png', color: '#00a53f', link: '/contacts', component: Contacts },
-  { id: 4, title: 'Гуртожиток', image: 'img/hostel.png', color: '#00a53f', link: '/dormitory', component: Dormitory },
-  { id: 5, title: 'Укриття', image: 'img/location.png', color: '#00a53f', link: '/shelter', component: Shelter },
+  {
+    id: 1,
+    title: "Аурієнту",
+    image: "img/abiturient.png",
+    color: "#00a53f",
+    link: "/admission",
+    component: Abiturientu,
+  },
+  {
+    id: 2,
+    title: "Студенту",
+    image: "img/student.png",
+    color: "#00a53f",
+    link: "/student",
+    component: Studentu,
+  },
+  {
+    id: 3,
+    title: "Контакти",
+    image: "img/telephone-call.png",
+    color: "#00a53f",
+    link: "/contacts",
+    component: Contacts,
+  },
+  {
+    id: 4,
+    title: "Гуртожиток",
+    image: "img/hostel.png",
+    color: "#00a53f",
+    link: "/dormitory",
+    component: Dormitory,
+  },
+  {
+    id: 5,
+    title: "Укриття",
+    image: "img/location.png",
+    color: "#00a53f",
+    link: "/shelter",
+    component: Shelter,
+  },
 ]);
 
 const handleNavigate = (link) => {
@@ -65,18 +97,14 @@ const openModal = (item) => {
   activeComponent.value = item.component;
   isModalOpen.value = true;
   // Блокуємо скрол основної сторінки
-  document.body.style.overflow = 'hidden';
+  document.body.style.overflow = "hidden";
 };
 
 const closeModal = () => {
   isModalOpen.value = false;
   activeComponent.value = null;
-  document.body.style.overflow = 'auto';
+  document.body.style.overflow = "auto";
 };
-
 </script>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
